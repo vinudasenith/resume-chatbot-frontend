0bf6e05dd992233bbb89fc6b74dc4c79f52c0e36
vinudasenith <ha.vinudas@gmail.com>
2025-09-16 21:35:02 +0530

    add more space between code lines✅

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,21 @@ import requests
 # Page configuration
 st.set_page_config(page_title="Chatbot Frontend", layout="centered")
 
+
 # Title
 st.title("🤖 AI Assistant")
+
 
 # add conversation state
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+
 # Display previous messages
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
+
 
 # Chat input
 if prompt := st.chat_input("Ask something..."):
@@ -22,6 +26,7 @@ if prompt := st.chat_input("Ask something..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+
 
     # Send user message to backend
     try:
@@ -37,9 +42,11 @@ if prompt := st.chat_input("Ask something..."):
     except Exception as e:
         bot_response = f"⚠️ Error: {e}"
 
+
     # Display ai response
     with st.chat_message("assistant"):
         st.markdown(bot_response)
+
 
     # Store ai response
     st.session_state.messages.append({"role": "assistant", "content": bot_response})
